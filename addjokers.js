2fa7cd3bd2ddfb89f83f2294713c4bcf3fb6e0ad
elbe <elbenews@gmail.com>
2024-10-07 14:21:16 -0500

    initial commic

---
+++ addjokers.js
@@ -0,0 +1,302 @@
+
+// works the same. 
+let consumables = [
+  // {
+  //   name: "Joker",
+  //   text: [
+  //     "{C:mult}+4{} Mult"
+  //   ],
+  //   image_url: "img/j_joker.png",
+  //   rarity: "Tarot"
+  // },
+  // {
+  //   name: "Joker",
+  //   text: [
+  //     "{C:mult}+4{} Mult"
+  //   ],
+  //   image_url: "img/j_joker.png",
+  //   rarity: "Planet"
+  // },
+  // {
+  //   name: "Joker",
+  //   text: [
+  //     "{C:mult}+4{} Mult"
+  //   ],
+  //   image_url: "img/j_joker.png",
+  //   rarity: "Spectral"
+  // },
+]
+
+let card_modifications = [
+  // {
+  //   name: "Joker",
+  //   text: [
+  //     "{C:mult}+4{} Mult"
+  //   ],
+  //   image_url: "img/j_joker.png",
+  //   rarity: "Enhancement"
+  // },
+  // {
+  //   name: "Joker",
+  //   text: [
+  //     "{C:mult}+4{} Mult"
+  //   ],
+  //   image_url: "img/j_joker.png",
+  //   rarity: "Edition"
+  // },
+  // {
+  //   name: "Joker",
+  //   text: [
+  //     "{C:mult}+4{} Mult"
+  //   ],
+  //   image_url: "img/sticker_example.png",
+  //   rarity: "Seal"
+  // },
+]
+
+let decks = [
+  // {
+  //   name: "Joker",
+  //   text: [
+  //     "{C:mult}+4{} Mult"
+  //   ],
+  //   image_url: "img/j_joker.png",
+  //   rarity: "Deck"
+  // },
+]
+
+let stickers = [
+  // {
+  //   name: "Joker",
+  //   text: [
+  //     "{C:mult}+4{} Mult"
+  //   ],
+  //   image_url: "img/sticker_example.png",
+  //   rarity: "Sticker"
+  // },
+]
+
+let blinds = [
+  // {
+  //   name: "The Wall",
+  //   text: [
+  //     "Extra large blind",
+  //     "{C:inactive}({C:red}4x{C:inactive} Base for {C:attention}$$$$${C:inactive})",
+  //     "{C:inactive}(Appears from Ante 2)"
+  //   ],
+  //   image_url: "img/the_wall.png",
+  //   rarity: "Boss Blind"
+  // },
+  // {
+  //   name: "Violet Vessel",
+  //   text: [
+  //     "Very large blind",
+  //     "{C:inactive}({C:red}6x{C:inactive} Base for {C:attention}$$$$$$$${C:inactive})",
+  //     "{C:inactive}(Appears from Ante 8)"
+  //   ],
+  //   image_url: "img/violet_vessel.png",
+  //   rarity: "Showdown"
+  // },
+]
+
+
+// let shop_items = [
+//   {
+//     name: "Joker",
+//     text: [
+//       "{C:mult}+4{} Mult"
+//     ],
+//     image_url: "img/j_joker.png",
+//     rarity: "Voucher"
+//   },
+//   {
+//     name: "Joker",
+//     text: [
+//       "{C:mult}+4{} Mult"
+//     ],
+//     image_url: "img/j_joker.png",
+//     rarity: "Pack"
+//   },
+// ]
+
+let cols = {
+  
+  MULT: "#FE5F55",
+  CHIPS: "#009dff",
+  MONEY: "#f3b958",
+  XMULT: "#FE5F55",
+  FILTER: "#ff9a00",
+  ATTENTION: "#ff9a00",
+  BLUE: "#009dff",
+  RED: "#FE5F55",
+  GREEN: "#4BC292",
+  PALE_GREEN: "#56a887",
+  ORANGE: "#fda200",
+  IMPORTANT: "#ff9a00",
+  GOLD: "#eac058",
+  YELLOW: "#ffff00",
+  CLEAR: "#00000000", 
+  WHITE: "#ffffff",
+  PURPLE: "#8867a5",
+  BLACK: "#374244",
+  L_BLACK: "#4f6367",
+  GREY: "#5f7377",
+  CHANCE: "#4BC292",
+  JOKER_GREY: "#bfc7d5",
+  VOUCHER: "#cb724c",
+  BOOSTER: "#646eb7",
+  EDITION: "#ffffff",
+  DARK_EDITION: "#5d5dff",
+  ETERNAL: "#c75985",
+  INACTIVE: "#ffffff99",
+  HEARTS: "#f03464",
+  DIAMONDS: "#f06b3f",
+  SPADES: "#403995",
+  CLUBS: "#235955",
+  ENHANCED: "#8389DD",
+  JOKER: "#708b91",
+  TAROT: "#a782d1",
+  PLANET: "#13afce",
+  SPECTRAL: "#4584fa",
+  VOUCHER: "#fd682b",
+  EDITION: "#4ca893",
+}
+
+let rarities = {
+  "Common": "#009dff", 
+  "Uncommon": "#4BC292",
+  "Rare": "#fe5f55",
+  "Legendary": "#b26cbb",
+  "Joker": "#708b91",
+  "Tarot": "#a782d1",
+  "Planet": "#13afce",
+  "Spectral": "#4584fa",
+  "Voucher": "#fd682b",
+  "Pack": "#9bb6bd",
+  "Enhancement": "#8389DD",
+  "Edition": "#4ca893",
+  "Seal": "#4584fa",
+  "Deck": "#9bb6bd",
+  "Sticker": "#5d5dff",
+  "Boss Blind": "#5d5dff",
+  "Showdown": "#4584fa",
+  "Junk": "#bfc7d5",
+  "The Seven Sins": "#7c0000",
+  "Twitch Series": "#9164ff",
+  "Icon Series": "#00FF99",
+  "Gaming Legends Series": "#7F00FF",
+  "Ritualistic": "#374244",
+  "Transcendent": "#ff9a00",
+  "Hypertranscendent": "#ffffff",
+  "O M E G A T R A N S C E N D E N T": "#374244",
+  "Epic": "#571d91",
+  "Evolved": "#eac058",
+  "Fusion": "#F7D762",
+  "Exotic": "#708b91",
+  "Fallen": "#8867a5",
+  "Divine": "#000000",
+}
+
+
+regex = /{([^}]+)}/g;
+
+let process_description = (description) => {
+  let desc_html = `<div class="text">`;
+  for (let i = 0; i < description.length; i++) {
+      if (i > 0) desc_html += "<br>"
+    for (let i2 = 0; i2 < description[i].length; i2++){
+      if (description[i][i2].background_colour){
+        desc_html += `<span style="background-color: ${description[i][i2].background_colour}; border-radius: 5px; padding: 0px 5px; color: ${description[i][i2].colour};">${description[i][i2].text}</span>`
+      }
+      else if (description[i][i2].colour == "#4f6367") {
+        desc_html += description[i][i2].text
+      }
+      else {
+        desc_html += `<span style="color: ${description[i][i2].colour};">${description[i][i2].text}</span>`
+      }
+    }
+  }
+
+  return desc_html += "</div>";
+}
+
+let add_cards_to_div = (jokers, jokers_div) => {
+  Object.keys(jokers).forEach(function(key,index) {
+    let joker = jokers[key]
+    console.log("adding joker", joker.name);
+  
+    let joker_div = document.createElement("div");
+    joker_div.classList.add("joker");
+    if (joker.rarity === "Sticker" || joker.rarity == "Seal") {
+      joker_div.innerHTML = `
+        <h3>${joker.name}</h3>
+        <img src="${joker.image_url}" alt="${joker.name}" class="hasback" />
+        <h4 class="rarity" style="background-color: ${rarities[joker.rarity]}">${joker.rarity}</h4>
+        ${process_description(joker.description)}
+      `;
+    } else {
+      joker_div.innerHTML = `
+        <h3>${joker.name}</h3>
+        <img src="${joker.image_url}" alt="${joker.name}" />
+        <h4 class="rarity" style="background-color: ${rarities[joker.rarity]}">${joker.rarity}</h4>
+        ${process_description(joker.description)}
+      `;
+    }
+    if (joker.mod){
+      joker_div.innerHTML += `
+      <h4 class="rarity" style="background-color: ${cards.Mods[joker.mod].badge_colour}">${cards.Mods[joker.mod].name}</h4>
+      `;
+    }
+    jokers_div.appendChild(joker_div);
+  
+});
+}
+
+if (cards.Joker.length === 0) {
+  document.querySelector(".jokersfull").style.display = "none"
+} else {
+  let jokers_div = document.querySelector(".jokers");
+  add_cards_to_div(cards.Joker, jokers_div);
+}
+
+if (consumables.length === 0) {
+  document.querySelector(".consumablesfull").style.display = "none"
+} else {
+  let consumables_div = document.querySelector(".consumables");
+  add_cards_to_div(consumables, consumables_div);
+}
+
+if (card_modifications.length === 0) {
+  document.querySelector(".cardmodsfull").style.display = "none"
+} else {
+  let cardmods_div = document.querySelector(".cardmods");
+  add_cards_to_div(card_modifications, cardmods_div);
+}
+
+if (decks.length === 0) {
+  document.querySelector(".decksfull").style.display = "none"
+} else {
+  let decks_div = document.querySelector(".decks");
+  add_cards_to_div(decks, decks_div);
+}
+
+if (stickers.length === 0) {
+  document.querySelector(".stickersfull").style.display = "none"
+} else {
+  let stickers_div = document.querySelector(".stickers");
+  add_cards_to_div(stickers, stickers_div);
+}
+
+if (blinds.length === 0) {
+  document.querySelector(".blindsfull").style.display = "none"
+} else {
+  let blinds_div = document.querySelector(".blinds");
+  add_cards_to_div(blinds, blinds_div);
+}
+
+if (shop_items.length === 0) {
+  document.querySelector(".shopitemsfull").style.display = "none"
+} else {
+  let shopitems_div = document.querySelector(".shopitems");
+  add_cards_to_div(shop_items, shopitems_div);
+}
