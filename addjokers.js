e1cd956dc05ab003a782437baa177e0b79ea588e
elbe <elbenews@gmail.com>
2024-10-10 08:34:17 -0500

    json import test

--- addjokers.js
+++ addjokers.js
@@ -326,3 +326,19 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+function fetchJSONData() {
+  fetch("./main.json")
+      .then((res) => {
+          if (!res.ok) {
+              throw new Error
+                  (`HTTP error! Status: ${res.status}`);
+          }
+          return res.json();
+      })
+      .then((data) =>
+          console.log(data))
+      .catch((error) =>
+          console.error("Unable to fetch data:", error));
+}
+fetchJSONData();
